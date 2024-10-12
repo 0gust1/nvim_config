0ac41e9f0da87035c59f72ecaf012300303c74eb
Augustin Chassine <augustin.chassine@mabsilico.com>
2024-10-12 17:45:36 +0200

    add shortcuts to navigate buffers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,13 +16,26 @@ keymap("v", "<S-i>", ":m .-2<CR>==", { desc = "Déplace le texte sélectionné v
 keymap("v", "<S-k>", ":m .+1<CR>==", { desc = "Déplace le texte sélectionné vers le bas en mode visuel" })
 
 -- I déplace le texte sélectionné vers le haut en mode visuel bloc (activé avec V)
-keymap("x", "<S-i>", ":move '<-2<CR>gv-gv", { desc = "Déplace le texte sélectionné vers le haut en mode visuel bloc" })
+keymap(
+	"x",
+	"<S-i>",
+	":move '<-2<CR>gv-gv",
+	{ desc = "Déplace le texte sélectionné vers le haut en mode visuel bloc" }
+)
 -- K déplace le texte sélectionné vers le bas en mode visuel (activé avec V)
-keymap("x", "<S-k>", ":move '>+1<CR>gv-gv", { desc = "Déplace le texte sélectionné vers le bas en mode visuel bloc" })
+keymap(
+	"x",
+	"<S-k>",
+	":move '>+1<CR>gv-gv",
+	{ desc = "Déplace le texte sélectionné vers le bas en mode visuel bloc" }
+)
+
+-- Navigation entre les buffers
+keymap("n", "<S-l>", ":bnext<CR>")
+keymap("n", "<S-h>", ":bprevious<CR>")
 
 -- Changement de fenêtre avec Ctrl + déplacement uniquement au lieu de Ctrl-w + déplacement
 keymap("n", "<C-h>", "<C-w>h", { desc = "Déplace le curseur dans la fenêtre de gauche" })
 keymap("n", "<C-j>", "<C-w>j", { desc = "Déplace le curseur dans la fenêtre du bas" })
 keymap("n", "<C-k>", "<C-w>k", { desc = "Déplace le curseur dans la fenêtre du haut" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Déplace le curseur dans la fenêtre droite" })
-
